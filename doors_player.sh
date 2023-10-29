8bd4dec41e94917a45ad5e578cce09532a17ede9
DonaldGelman <donaldgelman2@gmail.com>
2023-10-28 22:19:06 -0400

    filter

--- doors_player.sh
+++ doors_player.sh
@@ -1,2 +1,14 @@
 #!/bin/env bash
-mpv --prefetch-playlist --shuffle=yes --playlist="https://raw.githubusercontent.com/DonaldGelman/doors/main/doors_playlist.txt"
+
+playlist="https://raw.githubusercontent.com/DonaldGelman/doors/main/doors_playlist.txt"
+
+if [ "$#" -eq 0 ]
+then
+	mpv --prefetch-playlist --shuffle=yes --playlist=$playlist
+else
+	filter="$1"
+	filtered_pl=$HOME/bin/doors/filtered_pl.txt
+	curl -s $playlist | grep $filter > $filtered_pl
+	mpv --prefetch-playlist --shuffle=yes --playlist=$filtered_pl
+fi
+
